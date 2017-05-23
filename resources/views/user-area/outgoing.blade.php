189c523d3c683c3ef34c912ca80ff6c79955d5af
shegun babs <shegun.babs@gmail.com>
2017-05-24 00:29:27 +0100

    timer fix

--- resources/views/user-area/outgoing.blade.php
+++ resources/views/user-area/outgoing.blade.php
@@ -214,7 +214,7 @@
             var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
             var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
             var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            document.getElementById("demo").innerHTML = ' <span class="label label-info">' + days + "d </span> "
+            document.getElementById("{{$el['div']}}").innerHTML = ' <span class="label label-info">' + days + "d </span> "
                 + ' <span class="label label-info">' + hours + "h </span> "
                 + ' <span class="label label-info">' + minutes + "m </span> "
                 + ' <span class="label label-info">' + seconds + "s </span> ";
